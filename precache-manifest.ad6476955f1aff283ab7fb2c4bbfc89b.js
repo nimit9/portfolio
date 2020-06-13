a065a45e1c0c300169cb267251288f50fd02a9a0
Nimit Haria <nimitharia007@gmail.com>
2020-06-13 14:44:38 +0530

    Updates

---
+++ precache-manifest.ad6476955f1aff283ab7fb2c4bbfc89b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d584c69ae46488f1f184735546de662",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1611e715bbeffc75c654",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fcd39ed6a0e70742be04",
+    "url": "/portfolio/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "1611e715bbeffc75c654",
+    "url": "/portfolio/static/js/2.98e41c79.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/portfolio/static/js/2.98e41c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd39ed6a0e70742be04",
+    "url": "/portfolio/static/js/main.30102ccb.chunk.js"
+  },
+  {
+    "revision": "0ba229e925454d745cd9",
+    "url": "/portfolio/static/js/runtime-main.a090ea12.js"
+  },
+  {
+    "revision": "ace5d4d8b4c96abeee687aba6403448d",
+    "url": "/portfolio/static/media/Resume.ace5d4d8.pdf"
+  }
+]);
